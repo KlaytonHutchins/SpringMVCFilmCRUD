cb55e899d1a95738aaa5c908b7f05961501aaefc
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-10-08 16:02:39 -0400

    stubbed out controller for film keyword search

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,10 +1,11 @@
 package com.skilldistillery.film.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.data.FilmDAO;
@@ -24,5 +25,14 @@ public class FilmController {
 		mav.setViewName("film");
 		return mav;
 	}
-
+	
+	@RequestMapping(path = "showFilm.do", method = RequestMethod.GET, params = "filmWord")
+	public ModelAndView showFilm(String keyword) {
+		ModelAndView mav = new ModelAndView();
+		List<Film> films = filmDAO.findFilmByKeyword(keyword);
+		mav.addObject("films", films);
+		mav.setViewName("film");
+		return mav;
+	}
+	
 }
